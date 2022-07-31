a2756249210160a22f252d0abf3b5812a2eaecbf
notreallyaperson <xavinash931103@gmail.com>
2022-08-01 01:32:18 +0800

    Deploy: Mon  1 Aug 01:32:18 +08 2022 4d1d629323dd95b58559e22cc569b974f57ad9b2

--- src/components/about-13.jsx
+++ src/components/about-13.jsx
@@ -40,7 +40,11 @@ export const About13 = (props) => {
                       BYOB. We have some drinks but the more the merrier
                     </span></li>
                   </ul>
+<<<<<<< Updated upstream
                   <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15935.758103591907!2d101.598128!3d3.110703!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7d8b72057b88c0da!2sSoulCity%20Hub!5e0!3m2!1sen!2smy!4v1659286371142!5m2!1sen!2smy" style={{ border: "0", width: "100%", minHeight: '250px', marginTop: '1rem' }} className='paddingRight' allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+=======
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15935.758103591907!2d101.598128!3d3.110703!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7d8b72057b88c0da!2sSoulCity%20Hub!5e0!3m2!1sen!2smy!4v1659286371142!5m2!1sen!2smy"  style={{ border: "0", width: "100%", minHeight: '250px', marginTop: '1rem' }} className='paddingRight' allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+>>>>>>> Stashed changes
                 </div>
               </div>
             </div>
